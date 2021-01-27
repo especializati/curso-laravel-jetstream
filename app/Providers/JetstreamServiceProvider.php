4a26e06de3e8e56dcc3e8fd97aef247c287710b7
Carlos Ferreira <conta@especializati.com.br>
2021-01-27 10:59:42 -0300

    Add New Role

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -63,5 +63,9 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description(__('Editor users have the ability to read, create, and update.'));
+
+        Jetstream::role('support', __('Support'), [
+            'read',
+        ])->description(__('Support users have the ability to read.'));
     }
 }
